4b060b5eb45667e833c75e110a32969b738bdb4f
rutansh-07 <rutanshgovardhan07@gmail.com>
2025-06-19 15:31:24 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,11 @@
+
+// Write a program in java that prints "Hello World" followed by "I am starting my journey in Java!" on the next line.
+
+
+public class A
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World\nI am starting my journey in Java!");
+    }
+}
---
+++ B.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+// Main class named B
+public class B {
+    public static void main(String[] args) {
+        // Create an array 'a' to store 6 integers
+        int a[] = new int[6];
+
+        // Create a Scanner object to take input from the user
+        Scanner s1 = new Scanner(System.in);
+
+        // Loop to get 6 numbers from the user and store them in the array
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Enter the value of a[" + i + "]: "); // Ask user to enter a number
+            a[i] = s1.nextInt(); // Store the entered number in the array
+        }
+
+        // Print all the numbers entered by the user
+        System.out.println("The values you entered are:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]); // Show each number with its position
+        }
+
+        // Find the maximum number in the array
+        int m = 0; // Start with 0 as the maximum
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > m) { // If current number is bigger than current maximum
+                m = a[i]; // Update the maximum
+            }
+        }
+
+        // Print the largest number entered by the user
+        System.out.print("Maximum number in the array is: " + m);
+        s1.close(); // Close the scanner to free up resources
+    }
+}
